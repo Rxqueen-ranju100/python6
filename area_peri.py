bd269317d93e8762e063e958cb7aa0aea9b0ade1
Rxqueen-ranju100 <115641789+Rxqueen-ranju100@users.noreply.github.com>
2022-12-20 22:21:40 +0530

    Add files via upload

---
+++ area_peri.py
@@ -0,0 +1,6 @@
+from math import pi
+R=float(input("Enter radius of the circle:"))
+area=(pi*R*R)
+perimeter=(2*pi*R)
+print("The area of circle is","%.2f"%area)
+print("The perimeter of circle is","%.2f"%perimeter)
